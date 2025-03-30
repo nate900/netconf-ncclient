6ee166d581bd0ad0cec542f77cb990e05ebf310a
josiah <joemart152@yahoo.com>
2025-03-30 18:33:28 -0400

    Made a prompt in modify_if.py and get-if.py

--- get-if.py
+++ get-if.py
@@ -9,11 +9,13 @@ router = net_devs.get_homelab_4331()
 #http://cisco.com/ns/yang/Cisco-IOS-XE-native
 #?module=Cisco-IOS-XE-native&revision=2018-02-01
 #urn:ietf:params:xml:ns:yang:ietf-interfaces
-netconf_filter_if="""
+print("Enter the name of the interface you want: ")
+interface = input()
+netconf_filter_if=f"""
 <filter>
     <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
         <interface>
-            <name>GigabitEthernet0/0/0</name>
+            <name>{interface}</name>
         </interface>
     </interfaces>
 </filter>
@@ -30,7 +32,7 @@ native_filter = """
 """
 
 with manager.connect(host=router['host'], port=router['port'], username=router['username'], password=router['password'], hostkey_verify=False) as m:
-    response = m.get(native_filter)
+    response = m.get(netconf_filter_if)
     xmlDom = xml.dom.minidom.parseString(str(response))
     print(xmlDom.toprettyxml(indent='   '))
 
---
+++ modify_if.py
@@ -0,0 +1,32 @@
+from ncclient import manager
+import net_devs
+router = net_devs.get_homelab_4331()
+
+print("Enter a description: ")
+desc = input()
+
+loopback = f"""
+<config>
+    <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+        <interface>
+            <name>Loopback1000</name>
+            <description>{desc}</description>
+            <type xmlns:ianaift="urn:ietf:params:xml:ns:yang:iana-if-type">ianaift:softwareLoopback</type>
+            <enabled>true</enabled>
+            <ipv4 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">
+                <address>
+                    <ip>20.20.20.20</ip>
+                    <netmask>255.255.255.255</netmask>
+                </address>
+            </ipv4>
+            <ipv6 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip"/>
+        </interface>
+    </interfaces>
+</config>
+"""
+
+with manager.connect(host=router['host'], port=router['port'], username=router['username'], password=router['password'], hostkey_verify=False) as m:
+    data = m.edit_config(loopback, target='running')
+    print(data)
+
+
